d6223d02f505e52325180b4979b4d5b50f7f5231
Markus Neusinger <2921697+MarkusNeusinger@users.noreply.github.com>
2026-03-31 23:29:24 +0200

    fix: clean up formatting and improve downgrade function in remove_preview_thumb_column.py

--- alembic/versions/b833d85c09ed_remove_preview_thumb_column.py
+++ alembic/versions/b833d85c09ed_remove_preview_thumb_column.py
@@ -5,6 +5,7 @@ Revises: a2f4b8c91d23
 Create Date: 2026-03-31 23:07:22.910889
 
 """
+
 from typing import Sequence, Union
 
 import sqlalchemy as sa
@@ -13,8 +14,8 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b833d85c09ed'
-down_revision: Union[str, None] = 'a2f4b8c91d23'
+revision: str = "b833d85c09ed"
+down_revision: Union[str, None] = "a2f4b8c91d23"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -24,7 +25,4 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    op.add_column(
-        "impls",
-        sa.Column("preview_thumb", sa.String(), nullable=True),
-    )
+    op.add_column("impls", sa.Column("preview_thumb", sa.String(), nullable=True))
